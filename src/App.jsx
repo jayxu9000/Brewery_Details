66f43b4d92126e59a61798022b2d2326ebe88323
yxu60101 <yuanjiexu60101@gmail.com>
2023-10-12 02:03:15 -0400

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,108 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const URL = 'https://api.openbrewerydb.org/v1/breweries'
+  const [data, setData] = useState([]);
+  const [mostFrequentBrewery, setMostFrequentBrewery] = useState(null);
+  const [mostFrequentState, setMostFrequentState] = useState(null);
+  const [searchText, setSearchText] = useState('');
+  const [cityFilter, setCityFilter] = useState('');
+  const [stateFilter, setStateFilter] = useState('');
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(URL);
+        setData(response.data);
+
+        const breweryCounts = {};
+        const stateCounts = {};
+
+        response.data.forEach(brewery => {
+          if (breweryCounts[brewery.name]) {
+            breweryCounts[brewery.name]++;
+          } else {
+            breweryCounts[brewery.name] = 1;
+          }
+
+          if (stateCounts[brewery.state]) {
+            stateCounts[brewery.state]++;
+          } else {
+            stateCounts[brewery.state] = 1;
+          }
+        });
+
+        const mostFrequentBreweryName = Object.keys(breweryCounts).reduce(
+          (a, b) => (breweryCounts[a] > breweryCounts[b] ? a : b),
+          null
+        );
+
+        setMostFrequentBrewery(mostFrequentBreweryName);
+
+        const mostFrequentStateName = Object.keys(stateCounts).reduce(
+          (a, b) => (stateCounts[a] > stateCounts[b] ? a : b),
+          null
+        );
+
+        setMostFrequentState(mostFrequentStateName);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    if (data.length === 0) {
+      fetchData();
+    }
+  }, [data]);
+
+  const filteredData = data.filter((brewery) => {
+    const nameMatch = brewery.name.toLowerCase().includes(searchText.toLowerCase());
+    const cityMatch = brewery.city.toLowerCase().includes(cityFilter.toLowerCase());
+    const stateMatch = brewery.state.toLowerCase().includes(stateFilter.toLowerCase());
+    return nameMatch && cityMatch && stateMatch;
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <h1>List of Breweries</h1>
+      <div className='search-bar'>
+        <input
+          type='text'
+          placeholder='Search by brewery name'
+          value={searchText}
+          onChange={e => setSearchText(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Filter by city (try 'austin')"
+          value={cityFilter}
+          onChange={(e) => setCityFilter(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Filter by state (try 'texas')"
+          value={stateFilter}
+          onChange={(e) => setStateFilter(e.target.value)}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='summaryStats'>
+        <p>Number of Breweries: {data.length}</p>
+        {mostFrequentBrewery && (
+          <p>Most Frequent Brewery: {mostFrequentBrewery}</p>
+        )}
+        {mostFrequentState && (
+          <p>State with Most Breweries: {mostFrequentState}</p>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <ul>
+        {filteredData.map(brewery => (
+          <li key={brewery.id}>{brewery.name} ||| {brewery.city}, {brewery.state}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
