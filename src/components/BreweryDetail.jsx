4306608debcb399b397b56a971af8616746e1708
yxu60101 <yuanjiexu60101@gmail.com>
2023-10-22 21:27:55 -0400

    finished product

--- src/components/BreweryDetail.jsx
+++ src/components/BreweryDetail.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import '../App.css'
 
 function BreweryDetail({ data }) {
   const { id } = useParams();
-  const brewery = data.find(b => b.id === id);
+  const brewery = data.find(b => b.id.toString() === id);
   console.log('ID from useParams:', id);
   console.log('Data received in BreweryDetail:', data);
 
@@ -12,12 +13,16 @@ function BreweryDetail({ data }) {
   }
 
   return (
-    <div>
+    <div className='breweryDetails'>
+      <Link to="/">← Go Back</Link>
       <h2>Brewery Details</h2>
       <div>
         <p>Name: {brewery.name}</p>
         <p>City: {brewery.city}</p>
         <p>State: {brewery.state}</p>
+        <p>Address: {brewery.address_1}</p>
+        <p>Phone: {brewery.phone}</p>
+        <p>Website: {brewery.website_url}</p>
       </div>
     </div>
   );
